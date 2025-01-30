440a61e6bc973f5caf11152e8fcc24336448706e
minth <minthutatun@gmail.com>
2025-01-30 17:56:39 +0630

    message modified

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,9 @@
 <?php
 
 use App\Http\Controllers\MessageController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
 
     Route::get('/messages', [MessageController::class, 'index']);
     Route::post('/messages', [MessageController::class, 'store']);
