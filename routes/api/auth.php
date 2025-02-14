fec44b77612ff2904f210d148b7a0b55de36969f
minth <minthutatun@gmail.com>
2025-02-14 13:29:35 +0630

    user data api

--- routes/api/auth.php
+++ routes/api/auth.php
@@ -1,11 +1,17 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Resources\UserResource;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
+
+    Route::get('/user', function () {
+        return new UserResource(Auth::user());
+    });
 
     Route::post('/logout', [AuthController::class, 'destroy']);
 });
